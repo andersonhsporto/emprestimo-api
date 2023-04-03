892e38c771131fbd9a4dce9d8218ffaef51b7181
Anderson Porto <anderson.higo2@gmail.com>
2023-04-03 17:05:41 -0300

    add: toDto and toDtoList member functions

--- src/main/java/dev/anderson/emprestimoapi/mapper/LoanMapper.java
+++ src/main/java/dev/anderson/emprestimoapi/mapper/LoanMapper.java
@@ -6,13 +6,18 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface LoanMapper {
 
     LoanMapper INSTANCE = Mappers.getMapper(LoanMapper.class);
 
     @Mapping(target = "id", ignore = true)
-    @Mapping(target = "membership", ignore = true)
     @Mapping(target = "endValue", ignore = true)
     LoanEntity toModel(LoanDto loanDto);
+
+    LoanDto toDto(LoanEntity loanEntity);
+
+    List<LoanDto> toDtoList(List<LoanEntity> loanEntityList);
 }
--- src/main/java/dev/anderson/emprestimoapi/repositories/LoanRepository.java
+++ src/main/java/dev/anderson/emprestimoapi/repositories/LoanRepository.java
@@ -4,4 +4,6 @@ import dev.anderson.emprestimoapi.entities.LoanEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LoanRepository extends JpaRepository<LoanEntity, Long> {
+
+    boolean existsByIdAndAndCPFClient(Long id, String cpf);
 }
