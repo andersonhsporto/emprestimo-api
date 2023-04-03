e51a1066a93eff32ef821a2217505714a630eb0a
Anderson Porto <anderson.higo2@gmail.com>
2023-04-03 17:06:19 -0300

    add: get and delete member functions

--- src/main/java/dev/anderson/emprestimoapi/controller/LoanController.java
+++ src/main/java/dev/anderson/emprestimoapi/controller/LoanController.java
@@ -3,9 +3,11 @@ package dev.anderson.emprestimoapi.controller;
 import dev.anderson.emprestimoapi.dto.LoanDto;
 import dev.anderson.emprestimoapi.service.LoanService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -14,8 +16,28 @@ import javax.validation.Valid;
 public class LoanController {
 
     private LoanService loanService;
+
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public String makeLoan(@PathVariable String cpf, @Valid @RequestBody LoanDto loanDto) throws Exception {
         return loanService.makeLoan(cpf, loanDto);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteLoan(@PathVariable String cpf, @PathVariable Long id) throws Exception {
+        loanService.deleteLoan(cpf, id);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public LoanDto getLoan(@PathVariable String cpf, @PathVariable Long id) throws Exception {
+        return loanService.getLoan(cpf, id);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<LoanDto> getAllLoans(@PathVariable String cpf) throws Exception {
+        return loanService.getAllLoans(cpf);
     }
 }
