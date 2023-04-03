83e3f5409dc4d6a0a36b86d097262f30fa5b280d
Anderson Porto <anderson.higo2@gmail.com>
2023-04-03 01:23:42 -0300

    add: makeClient member function

--- src/main/java/dev/anderson/emprestimoapi/service/ClientService.java
+++ src/main/java/dev/anderson/emprestimoapi/service/ClientService.java
@@ -1,5 +1,8 @@
 package dev.anderson.emprestimoapi.service;
 
+import dev.anderson.emprestimoapi.dto.ClientDto;
+import dev.anderson.emprestimoapi.entities.ClientEntity;
+import dev.anderson.emprestimoapi.mapper.ClientMapper;
 import dev.anderson.emprestimoapi.repositories.ClientRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,6 +12,14 @@ import org.springframework.stereotype.Service;
 public class ClientService {
 
     private ClientRepository clientRepository;
+
+    private ClientMapper clientMapper;
+
+    public String makeClient(ClientDto clientDto) {
+        ClientEntity clientEntity = clientMapper.toModel(clientDto);
+        clientRepository.save(clientEntity);
+        return clientDto.toString();
+    }
 
 
 }
