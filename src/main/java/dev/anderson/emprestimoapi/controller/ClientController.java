cae5a3c1ec8f7e374edb2367e10f5e8399513da7
Anderson Porto <anderson.higo2@gmail.com>
2023-04-03 01:24:09 -0300

    refactor: using AllArgsConstructor

--- src/main/java/dev/anderson/emprestimoapi/controller/ClientController.java
+++ src/main/java/dev/anderson/emprestimoapi/controller/ClientController.java
@@ -1,22 +1,24 @@
 package dev.anderson.emprestimoapi.controller;
 
+import dev.anderson.emprestimoapi.dto.ClientDto;
 import dev.anderson.emprestimoapi.service.ClientService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/clientes")
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class ClientController {
 
     private ClientService clientService;
 
-    @GetMapping
-    public String hello() {
-        return "Hello World";
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String makeClient(@Valid @RequestBody ClientDto clientDto) {
+        return clientService.makeClient(clientDto);
     }
 }
