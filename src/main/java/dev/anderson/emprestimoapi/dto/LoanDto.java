aeb7a28e8622da3960407df0f32fef8e727432aa
Anderson Porto <anderson.higo2@gmail.com>
2023-04-03 02:40:48 -0300

    refactor: dto struct

---
+++ src/main/java/dev/anderson/emprestimoapi/dto/LoanDto.java
@@ -0,0 +1,45 @@
+package dev.anderson.emprestimoapi.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import dev.anderson.emprestimoapi.types.Membership;
+import lombok.Data;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+public class LoanDto {
+
+    @JsonProperty("CPFCliente")
+    @CPF
+    private String CPFClient;
+
+    @JsonProperty("ValorInicial")
+    @NotNull(message = "O campo 'ValorInicial' é obrigatório")
+    private BigDecimal startValue;
+
+    @JsonProperty("dataInicio")
+    private LocalDateTime startDateTime;
+
+    @JsonProperty("dataFinal")
+    private LocalDateTime endDateTime;
+
+    private Membership membership;
+
+    private BigDecimal endValue;
+
+    @Override
+    public String toString() {
+        return "LoanDto{" +
+                "CPFClient='" + CPFClient + '\'' +
+                ", startValue=" + startValue +
+                ", startDateTime=" + startDateTime +
+                ", endDateTime=" + endDateTime +
+                ", membership=" + membership +
+                ", endValue=" + endValue +
+                '}';
+    }
+}
