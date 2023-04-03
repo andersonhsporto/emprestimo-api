58baaa1a1012892f9825603c5ed137febd1e4eb9
Anderson Porto <anderson.higo2@gmail.com>
2023-04-03 17:05:21 -0300

    refactor: using code in constructor

--- src/main/java/dev/anderson/emprestimoapi/types/Membership.java
+++ src/main/java/dev/anderson/emprestimoapi/types/Membership.java
@@ -6,28 +6,34 @@ public enum Membership {
 
     Bronze(1) {
         @Override
-        public BigDecimal getMembershipStatus(BigDecimal salary) {
-            return salary.multiply(BigDecimal.valueOf(0.2));
+        public BigDecimal getMembershipStatus(BigDecimal startValue, Integer numberOfLoans) {
+            return startValue.multiply(BigDecimal.valueOf(1.8));
         }
     },
     Silver(2) {
         @Override
-        public BigDecimal getMembershipStatus(BigDecimal salary) {
-            return salary.multiply(BigDecimal.valueOf(0.4));
+        public BigDecimal getMembershipStatus(BigDecimal startValue, Integer numberOfLoans) {
+            if (startValue.compareTo(BigDecimal.valueOf(5000)) > 0) {
+                return startValue.multiply(BigDecimal.valueOf(1.5));
+            } else {
+                return startValue.multiply(BigDecimal.valueOf(1.6));
+            }
         }
     },
     Gold(3) {
         @Override
-        public BigDecimal getMembershipStatus(BigDecimal salary) {
-            return salary.multiply(BigDecimal.valueOf(0.6));
+        public BigDecimal getMembershipStatus(BigDecimal startValue, Integer numberOfLoans) {
+            if (numberOfLoans == 1) {
+                return startValue.multiply(BigDecimal.valueOf(1.2));
+            } else {
+                return startValue.multiply(BigDecimal.valueOf(1.3));
+            }
         }
     };
 
-    private int code;
     private Membership(int code) {
-        this.code = code;
     }
 
-    public abstract BigDecimal getMembershipStatus(BigDecimal salary);
+    public abstract BigDecimal getMembershipStatus(BigDecimal salary, Integer numberOfLoans);
 
 }
