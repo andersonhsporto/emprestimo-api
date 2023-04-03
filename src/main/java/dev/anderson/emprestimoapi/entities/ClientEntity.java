3083f25cd1ae34d9cdcabb7be5f7e3d9ed1ad185
Anderson Porto <anderson.higo2@gmail.com>
2023-04-03 01:23:59 -0300

    add: accessors annotation

--- src/main/java/dev/anderson/emprestimoapi/entities/ClientEntity.java
+++ src/main/java/dev/anderson/emprestimoapi/entities/ClientEntity.java
@@ -1,14 +1,18 @@
 package dev.anderson.emprestimoapi.entities;
 
-import dev.anderson.emprestimoapi.types.Membership;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
-
-import org.hibernate.validator.constraints.br.CPF;
-
 import java.math.BigDecimal;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class ClientEntity {
 
     @Id
@@ -17,12 +21,24 @@ public class ClientEntity {
 
     private String name;
 
-    @CPF
     private String cpf;
 
     private String telephone;
+
     private BigDecimal salary;
 
     @OneToOne(cascade = CascadeType.ALL)
     private AddressEntity address;
+
+    @Override
+    public String toString() {
+        return "ClientEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", telephone='" + telephone + '\'' +
+                ", salary=" + salary +
+                ", address=" + address +
+                '}';
+    }
 }
