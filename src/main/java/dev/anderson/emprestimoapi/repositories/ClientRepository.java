4ff82bbcfe478bb6df436713c151a9f059409d77
Anderson Porto <anderson.higo2@gmail.com>
2023-04-03 01:51:32 -0300

    add: existsByCpf member function

--- src/main/java/dev/anderson/emprestimoapi/repositories/ClientRepository.java
+++ src/main/java/dev/anderson/emprestimoapi/repositories/ClientRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientRepository extends JpaRepository<ClientEntity, Long> {
     ClientEntity findByCpf(String cpf);
+
+    boolean existsByCpf(String cpf);
 }
