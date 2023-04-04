9aed99c3faf5ee6b256317daf4ce48f402c7f05e
Anderson Porto <anderson.higo2@gmail.com>
2023-04-04 15:12:58 -0300

    refactor: removal unused validation

--- src/main/java/dev/anderson/emprestimoapi/entities/AddressEntity.java
+++ src/main/java/dev/anderson/emprestimoapi/entities/AddressEntity.java
@@ -9,8 +9,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
 
 @Entity
 @AllArgsConstructor
@@ -23,14 +21,10 @@ public class AddressEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotEmpty(message = "O campo 'rua' é obrigatório")
     private String street;
 
-    @NotEmpty(message = "O campo 'número' é obrigatório")
     private String number;
 
-    @NotEmpty(message = "O campo 'CEP' é obrigatório")
-    @Pattern(regexp = "\\d{5}-\\d{3}")
     private String zipCode;
 
 }
