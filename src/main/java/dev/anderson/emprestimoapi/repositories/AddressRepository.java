d891d7c67a25095f58674d674c85c9c7b3729a20
Anderson Porto <anderson.higo2@gmail.com>
2023-04-04 15:12:07 -0300

    delete: unused repository

--- src/main/java/dev/anderson/emprestimoapi/repositories/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.anderson.emprestimoapi.repositories;
-
-import dev.anderson.emprestimoapi.entities.AddressEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<AddressEntity, Long> {
-}
