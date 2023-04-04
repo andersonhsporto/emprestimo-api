6b6145054971dba71c0da22cc0d34ebe7e6e30cd
Anderson Porto <anderson.higo2@gmail.com>
2023-04-04 15:15:55 -0300

    refactor: using bad request http status code

--- src/main/java/dev/anderson/emprestimoapi/exceptions/MaxLoanException.java
+++ src/main/java/dev/anderson/emprestimoapi/exceptions/MaxLoanException.java
@@ -3,7 +3,7 @@ package dev.anderson.emprestimoapi.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.CONFLICT)
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class MaxLoanException extends Exception {
     public MaxLoanException(String message) {
         super("Este Empréstimo ultrapassa o limite do cliente: " + message);
