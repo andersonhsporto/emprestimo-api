818bc25e59f5fe4ed2c87fa004bc40cc8eec0917
Anderson Porto <anderson.higo2@gmail.com>
2023-04-14 02:41:24 -0300

    add: validations on address entity

--- src/main/java/dev/anderson/emprestimoapi/entities/AddressEntity.java
+++ src/main/java/dev/anderson/emprestimoapi/entities/AddressEntity.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @AllArgsConstructor
@@ -21,10 +22,13 @@ public class AddressEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     private String street;
 
+    @NotNull
     private String number;
 
+    @NotNull
     private String zipCode;
 
 }
