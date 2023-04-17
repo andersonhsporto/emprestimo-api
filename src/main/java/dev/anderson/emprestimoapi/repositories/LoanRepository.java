6a872eb788ba0473876ff633868ba033f38898bf
Anderson Porto <anderson.higo2@gmail.com>
2023-04-17 00:39:37 -0300

    refactor: cleaning code

--- src/main/java/dev/anderson/emprestimoapi/repositories/LoanRepository.java
+++ src/main/java/dev/anderson/emprestimoapi/repositories/LoanRepository.java
@@ -3,8 +3,6 @@ package dev.anderson.emprestimoapi.repositories;
 import dev.anderson.emprestimoapi.entities.LoanEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface LoanRepository extends JpaRepository<LoanEntity, Long> {
 
     boolean existsByIdAndAndCpfClient(Long id, String cpf);
